dc3d7d5e96f048c8c1b70b795d5a7f091fbb5874
Rory Sullivan <rorysullivan@u.boisestate.edu>
2020-04-22 20:33:57 -0600

    organized plot.py so all the coding is together and easier to see

--- plot.py
+++ plot.py
@@ -5,22 +5,6 @@ import sys
 filename = sys.argv[1]        # Stores ARG1 in filename, as in: $ python plot.py ARG1 ARG2 
 data = np.loadtxt(filename,skiprows=32,delimiter=",")   # Attempts to load filename into local variable data.
 
-## Part 0
-# Figure out what arguments to add to the loadtxt function call
-# so that numbers are loaded into the local function 'data'.
-# Hint: look for arguments like 'skiprows' and 'delimiter'
-# Check by running:
-#   $ python plot.py raw-data/Sp15_245L_sect-001_group-1_glass.raw
-# at the command line.
-
-
-## Part 1
-# Figure out what columns and rows of data we need to plot
-# Stress (y-axis) vs Strain (x-axis)
-# plot raw-data/Sp15_245L_sect-001_group-1_glass.raw
-# Make sure to include axis labels and units!
-# plt.plot(xdata,ydata, arguments-to-make-plot-pretty)
-
 Stress= data[:,3]
 Strain= data[:,7]
 plt.plot(-Strain,-Stress,color="r",linestyle="-",label="data")
@@ -36,6 +20,22 @@ plt.show()
 print("Youngs Modulus =")
 print(Slope)
 print("MPa")
+
+## Part 0
+# Figure out what arguments to add to the loadtxt function call
+# so that numbers are loaded into the local function 'data'.
+# Hint: look for arguments like 'skiprows' and 'delimiter'
+# Check by running:
+#   $ python plot.py raw-data/Sp15_245L_sect-001_group-1_glass.raw
+# at the command line.
+
+## Part 1
+# Figure out what columns and rows of data we need to plot
+# Stress (y-axis) vs Strain (x-axis)
+# plot raw-data/Sp15_245L_sect-001_group-1_glass.raw
+# Make sure to include axis labels and units!
+# plt.plot(xdata,ydata, arguments-to-make-plot-pretty)
+
 
 ## Part 2
 # Check to see if your code in part 1 will plot all of the files in raw-data/
